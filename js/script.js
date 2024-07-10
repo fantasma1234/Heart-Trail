12a208c73e6a05b86cab7cb02c2521eea0507a86
fantasma1234 <guilhermedroni07@gmail.com>
2024-07-09 23:47:22 -0300

    teste de touchmove

--- js/script.js
+++ js/script.js
@@ -22,3 +22,26 @@ body.addEventListener("mousemove", (event) => {
         span.remove()
     }, 3000)
 })
+
+body.addEventListener("touchmove", (event) => {
+    //console.log(event.offsetX);
+    //pega a posição do mouse na tela
+    const posX = event.offsetX
+    const posY = event.offsetY
+    const span = document.createElement('span')
+    span.style.left = posX + 'px'
+    span.style.top = posY + 'px'
+
+    //muda o tamanho dos corações
+    const tamanho = Math.random() * 100
+    span.style.width = tamanho + 'px'
+    span.style.height = tamanho + 'px'
+
+    //adiciona o span dentro do body
+    body.appendChild(span)
+
+    //marca o tempo de duração do span, nesse caso 3s
+    setTimeout(() => {
+        span.remove()
+    }, 3000)
+})
