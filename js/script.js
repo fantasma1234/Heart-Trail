ba847f7996ec4a6e74fdbe3a7deaa5588b24d1bb
fantasma1234 <guilhermedroni07@gmail.com>
2024-07-10 00:38:11 -0300

    Adicionando touchmove

--- js/script.js
+++ js/script.js
@@ -24,24 +24,25 @@ body.addEventListener("mousemove", (event) => {
 })
 
 body.addEventListener("touchmove", (event) => {
-    //console.log(event.offsetX);
-    //pega a posição do mouse na tela
-    const posX = event.offsetX
-    const posY = event.offsetY
-    const span = document.createElement('span')
-    span.style.left = posX + 'px'
-    span.style.top = posY + 'px'
+    console.log(event);
+    [...event.changedTouches].forEach(touch => {
+        //pega a posição do dedo na tela
+        const span = document.createElement('span')
+        span.style.left = `${touch.pageX}px`
+        span.style.top =  `${touch.pageY}px`
+        span.id = touch.identifier
 
-    //muda o tamanho dos corações
-    const tamanho = Math.random() * 100
-    span.style.width = tamanho + 'px'
-    span.style.height = tamanho + 'px'
+        //muda o tamanho dos corações
+        const tamanho = Math.random() * 100
+        span.style.width = tamanho + 'px'
+        span.style.height = tamanho + 'px'
 
-    //adiciona o span dentro do body
-    body.appendChild(span)
+        //adiciona o span dentro do body
+        body.appendChild(span)
 
-    //marca o tempo de duração do span, nesse caso 3s
-    setTimeout(() => {
-        span.remove()
-    }, 3000)
+        //marca o tempo de duração do span, nesse caso 3s
+        setTimeout(() => {
+            span.remove()
+        }, 3000)
+    })
 })
