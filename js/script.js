4f9553c2d0cb08581572f0ce0e073a5c15e4bc20
fantasma1234 <guilhermedroni07@gmail.com>
2024-07-09 23:21:28 -0300

    comentários

--- js/script.js
+++ js/script.js
@@ -2,15 +2,22 @@ const body = document.querySelector("body");
 
 body.addEventListener("mousemove", (event) => {
     //console.log(event.offsetX);
+    //pega a posição do mouse na tela
     const posX = event.offsetX
     const posY = event.offsetY
     const span = document.createElement('span')
     span.style.left = posX + 'px'
     span.style.top = posY + 'px'
+
+    //muda o tamanho dos corações
     const tamanho = Math.random() * 100
     span.style.width = tamanho + 'px'
     span.style.height = tamanho + 'px'
+
+    //adiciona o span dentro do body
     body.appendChild(span)
+
+    //marca o tempo de duração do span, nesse caso 3s
     setTimeout(() => {
         span.remove()
     }, 3000)
